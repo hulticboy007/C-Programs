06d252843014af23a84044074293a46af7b2ac24
harshedabdulla <harshed2004@gmail.com>
2022-08-06 22:53:34 +0530

    not working

---
+++ ch-2/pb19.c
@@ -0,0 +1,25 @@
+/** Program to print sequence star 2
+ * consider n=5
+ * ALGORITHM
+ * 
+ * START
+ * 
+ * 
+ * 
+ * STOP
+* */
+#include<stdio.h>
+int main(){
+ for (int n = 1; n < 5; n++)
+ {
+    printf(" ");
+    for (int j = 1; j < n; j++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    
+ }
+ 
+
+}
