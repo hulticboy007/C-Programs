778591b247e56877bbf548add09748dc2ee05592
harshedabdulla <harshed2004@gmail.com>
2022-08-06 23:34:25 +0530

    finally done it, good problem

--- ch-2/pb19.c
+++ ch-2/pb19.c
@@ -12,17 +12,18 @@
 int main(){
  for (int i = 1; i <= 5; i++)
  {
-    for (int j = 5; j >= i; j--)
+    for (int j = 1; j <= 5; j++)
     {
-        printf(" ");
-        for (int k = 1; k <= i; k++)
+        if (i+j<=5)
         {
+            /* code */
+            printf(" ");
+        }else{
             printf("*");
-            
         }
-        
-    }printf("\n");
- 
+          
+    }
+ printf("\n");
  }
 
 }
