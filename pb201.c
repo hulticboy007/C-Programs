a504e503fce65eaf5e109213a6014cdc59efa786
harshedabdulla <harshed2004@gmail.com>
2022-08-07 12:41:23 +0530

    complete

--- pb201.c
+++ pb201.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main(){
-    for (int i = 1; i <= 5; i++)//rows
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    for (int i = 1; i <= n; i++)//rows
     {
-        for (int j = 1; j <= 5-i; j++)//spaces
+        for (int j = 1; j <=(n-i); j++)//spaces
         {
-            printf(" ");
-            for (int x = 1; x <= (2*i-1); x++)
-            {
+                printf(" ");        
+        }
+        for (int x=1; x<=(2*i-1);x++)
+        {
                 printf("*");    
-            }
-         
-        }printf("\n"); 
-
-        
-    }  
-    
+        }
+    printf("\n");
+    }
 }
