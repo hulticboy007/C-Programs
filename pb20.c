6e15d6ae3e6bfb744adea7466c81d3ebf37cb6ef
harshedabdulla <harshed2004@gmail.com>
2022-08-07 11:40:37 +0530

    completed

--- pb20.c
+++ pb20.c
@@ -5,10 +5,26 @@
  * START
  * n=5
  * 
- * 
+ *
  * STOP
 * */
 #include<stdio.h>
 int main(){
-
+    int row;
+    printf("Enter the number of rows: ");
+    scanf("%d",&row);
+    
+ for (int i = 0; i < row; i++)
+ {
+    for (int s = 0; s < row-i; s++)
+    {
+        printf(" ");
+    }
+    for (int j = 0; j < i; j++)
+    {
+        printf("* ");
+    }
+        printf("\n");
+ }
+ 
 }
