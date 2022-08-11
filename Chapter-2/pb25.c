a68738e1eb7efe99d7bef65ed3c5a064ccf3423b
harshedabdulla <harshed2004@gmail.com>
2022-08-11 08:19:42 +0530

    pb25

---
+++ Chapter-2/pb25.c
@@ -0,0 +1,24 @@
+/** Program to sum of series of 1+3+5+7+....
+ * 
+ * ALGORITHM
+ * 
+ * START
+ * print enter the value till you which you need series
+ * input n
+ * 
+ * STOP
+* */
+#include<stdio.h>
+int main(){
+    int n,s=0;
+    printf("Enter the values: ");
+    scanf("%d",&n);
+    for (int i = 1; i <= n; i=i+2)
+    {
+        /* code */
+        printf("%d+",i);
+        s=s+i;
+    }
+        printf("%d",s);
+        return 0;
+}
