0315cb23a48e1501f51fd7f8df97965ed154aec9
harshedabdulla <harshed2004@gmail.com>
2022-08-13 21:59:33 +0530

    pb1

---
+++ Arrays/pb1.c
@@ -0,0 +1,27 @@
+/** Program to input numbers and print 
+ * 
+ * ALGORITHM
+ * 
+ * START
+ * 
+ * 
+ * 
+ * STOP
+* */
+#include<stdio.h>
+int main(){
+    int a[5];
+    printf("Enter 5 numbers: ");
+    for (int i = 1; i <=5; i++) 
+    {
+        /* code */
+        scanf("%d",&a[i]);
+    }
+    for (int j = 1; j <= 5; j++)
+    {
+        /* code */
+        printf("%d\n",a[j]);
+    }
+    
+    
+}
