06746b7461576cea79154f2924a697ebb0c005f7
harshedabdulla <harshed2004@gmail.com>
2022-08-17 22:21:43 +0530

    pb1

---
+++ Arrays/2-D Array/pb1.c
@@ -0,0 +1,38 @@
+/** Program to input a 2d array and output it
+ * 
+ * ALGORITHM
+ * 
+ * START
+ * 
+ * 
+ * 
+ * STOP
+* */
+#include<stdio.h>
+int main(){
+int arr[2][3];
+for (int i = 0; i < 2; i++)
+{
+    /* code */
+    for (int j = 0; j < 3; j++)
+    {
+        /* code */
+        scanf("%d",&arr[i][j]);
+    }
+    
+}
+
+for (int i = 0; i < 2; i++)
+{
+    /* code */
+    for (int j = 0; j < 3; j++)
+    {
+        /* code */
+        printf("The %d %d value of the array arr[%d][%d] is %d :\n",i,j,i,j,arr[i][j]);
+    }
+    
+}
+
+
+
+}
