595685c5c6a0ec5b26300682fc452cc28a34bcb4
harshedabdulla <harshed2004@gmail.com>
2022-08-18 19:59:39 +0530

    pb30 not working

--- TCS-NQT/Chapter-2/pb30.c
+++ TCS-NQT/Chapter-2/pb30.c
@@ -1,24 +1,17 @@
 /** Program to find the maximum of an array
- * 
- * ALGORITHM
- * 
- * START
- * 
- * 
- * 
- * STOP
+
 * */
 #include<stdio.h>
 int main(){
 int a[5],max,i;
 printf("Enter the values of the array: ");
 max=a[0];
-for (int i = 0; i < 5; i++)
+for (i = 0; i < 5; i++)
 {
     /* code */
     scanf("%d",&a[i]);
 }
-for (int i = 1; i < 5; i++)
+for (i = 1; i < 5; i++)
 {
     /* code */
     if (max<a[i])
