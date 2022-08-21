ce4840d8260e160640f0f1dc25e6bd7808af21fe
harshedabdulla <harshed2004@gmail.com>
2022-08-21 23:22:19 +0530

    pb33

---
+++ TCS-NQT/Chapter-2/pb33.c
@@ -0,0 +1,76 @@
+// Program to subtract two matrices
+
+
+#include<stdio.h>
+int main(){
+    int a[3][3],i,j,b[3][3],s[3][3];
+    printf("Enter the first matrix: \n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("Enter the a[%d][%d] element: ",i,j);
+            scanf("%d",&a[i][j]);
+            printf("\n");
+        }
+        
+    }
+    printf("Enter the second matrix: \n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("Enter the a[%d][%d] element: ",i,j);
+            scanf("%d",&b[i][j]);
+            printf("\n");
+        }
+    
+    }
+
+    printf("first matrix: \n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d\t",a[i][j]);
+            
+            
+        }
+        printf("\n");
+    }
+
+    printf("second matrix: \n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d\t",b[i][j]);
+            
+            
+        }
+        printf("\n");
+    }
+
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            s[i][j]=a[i][j]-b[i][j];
+            
+            
+        }
+        
+    }
+    printf("subtracted matrix: \n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d\t",s[i][j]);
+            
+            
+        }
+        printf("\n");
+    }
+}
