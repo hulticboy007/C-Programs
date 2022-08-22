2f60d8dcdad843ba6248332355ee5e9f713aa51b
harshedabdulla <harshed2004@gmail.com>
2022-08-22 19:51:00 +0530

    changez

--- Arrays/Linear Search/pb2.c
+++ Arrays/Linear Search/pb2.c
@@ -4,15 +4,38 @@
 
 #include<stdio.h>
 int main(){
-    int arr[6];
+    int arr[6],i,n,found;
     for (i = 0; i < 6; i++)
     {
         printf("Enter the %d value in arr[%d]: ",i,i);
         scanf("%d",&arr[i]);
     }
-    printf("The array values entered are: ");
+    printf("arr: ");
     for (i = 0; i < 6; i++)
     {
         printf("%d,",arr[i]);
     }
+    printf("Enter the value to be searched: ");
+    scanf("%d",&n);
+    printf("%d is the value to be searched from the array\n",n);
+
+
+    for ( i = 0; i < 6; i++)
+    {
+        /* code */
+        if (arr[i]==n)
+        {
+            /* code */
+            found=1;
+            break;
+        }
+    }
+    if (found==1)
+    
+        /* code */
+        printf("Found\n");
+    else
+        printf("Not found\n");
+    
+
 }
