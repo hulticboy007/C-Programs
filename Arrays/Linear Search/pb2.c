11da364e9366ba85cf6b8582a95b2324777b1521
harshedabdulla <harshed2004@gmail.com>
2022-08-22 19:43:36 +0530

    modifications

--- Arrays/Linear Search/pb2.c
+++ Arrays/Linear Search/pb2.c
@@ -1,12 +1,9 @@
-// Program to find through an array and give the index
+// C program to input N numbers and store them in an array.
+// Do a linear search for a given key and report success or failure.
 
 
 #include<stdio.h>
 int main(){
-    int n,i;
-    printf("Enter the value to be searched: ");
-    scanf("%d",&n);
-    printf("%d is the value to be searched from the array\n",n);
     int arr[6];
     for (i = 0; i < 6; i++)
     {
@@ -18,18 +15,4 @@ int main(){
     {
         printf("%d,",arr[i]);
     }
-    printf("\n");
-    for ( i = 0; i < 6; i++)
-    {
-        /* code */
-        if (arr[i]==n)
-        {
-            /* code */
-            printf("%d Value is found!\n",arr[i]);
-        }else{
-            printf("%d Value is not found!\n",arr[i]);
-        }
-        
-    }
-    
 }
