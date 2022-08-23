7a96e7c9ba1dec7b8cffb49025bc0ce4191f4813
harshedabdulla <harshed2004@gmail.com>
2022-08-23 16:40:40 +0530

    pb3

---
+++ Arrays/Linear Search/pb3.c
@@ -0,0 +1,19 @@
+// Program to search through an array and output the index where it is located
+
+
+#include<stdio.h>
+int main(){
+
+    int n,i,j,arr[6];
+    for (i = 0; i < 6; i++)
+    {
+        printf("Enter the %d value: ",i);
+        scanf("%d",&arr[i]);
+        printf("\n");
+    }
+    printf("\n");
+    printf("Enter the value to be searched: ");
+    scanf("%d",&n);
+    printf("\n");
+    
+}
