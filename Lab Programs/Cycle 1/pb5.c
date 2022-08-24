d7ef57afcd74a00fb57c10d6246f2c5741d9834c
harshedabdulla <harshed2004@gmail.com>
2022-08-24 22:02:46 +0530

    pb5

--- Lab Programs/Cycle 1/pb5.c
+++ Lab Programs/Cycle 1/pb5.c
@@ -3,12 +3,25 @@
  * ALGORITHM
  * 
  * START
- * 
- * 
- * 
+ * read the num
+ * m=num%10
+ * r=r+m
+ * repeat until m=0
  * STOP
 * */
 #include<stdio.h>
 int main(){
-    
+    int num,r=0,m;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+    while (num>0)
+    {
+        /* code */
+        m=num%10;
+        r=r+m;
+        num=num/10;
+    }
+    printf("The sum is %d",r);
+    return 0;
+
 }
