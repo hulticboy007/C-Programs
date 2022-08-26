ec3ceb4299050111086f680222b52e23234afab6
harshedabdulla <harshed2004@gmail.com>
2022-08-26 08:15:08 +0530

    pb12

--- Lab Programs/Cycle 2/Arrays/pb12.c
+++ Lab Programs/Cycle 2/Arrays/pb12.c
@@ -10,17 +10,20 @@
 * */
 #include<stdio.h>
 int main(){
-    int arr[6],sum=0,avg;
-    for (int i = 0; i < 6; i++)
+    int sum=0,avg,k;
+    printf("Enter the elements in the array: ");
+    scanf("%d",&k);
+    int arr[k];
+    for (int i = 0; i < k; i++)
     {
         printf("Enter the %dth element: ",i);
         scanf("%d",&arr[i]);
     }
-    for (int j = 0; j < 6; j++)
+    for (int j = 0; j < k; j++)
     {
         sum=sum+arr[j];
     }
-    avg=sum/6;
+    avg=sum/k;
     printf("Sum is %d\n",sum);
     printf("Average is %d\n",avg);
 }
