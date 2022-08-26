c3f9a92095ee18dd944a2a468f60cc0c3c1d8484
harshedabdulla <harshed2004@gmail.com>
2022-08-26 08:10:41 +0530

    hgh

--- Lab Programs/Cycle 2/Arrays/pb12.c
+++ Lab Programs/Cycle 2/Arrays/pb12.c
@@ -10,11 +10,17 @@
 * */
 #include<stdio.h>
 int main(){
-    int arr[6];
+    int arr[6],sum=0,avg;
     for (int i = 0; i < 6; i++)
     {
         printf("Enter the %dth element: ",i);
         scanf("%d",&arr[i]);
     }
-    
+    for (int j = 0; j < 6; j++)
+    {
+        sum=sum+arr[j];
+    }
+    avg=sum/6;
+    printf("Sum is %d\n",sum);
+    printf("Average is %d\n",avg);
 }
