b0022cd2bc8c2a351e41e6c7d89d2e728ea88bf7
harshedabdulla <harshed2004@gmail.com>
2022-08-26 20:23:10 +0530

    dfsdf

---
+++ Arrays/Binary Search/pb1.c
@@ -0,0 +1,16 @@
+// Program to search for an element using binary search
+
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    //if elements are not sorted
+
+}
