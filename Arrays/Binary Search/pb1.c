05d59c3f64ede46c6fb932cee447589f902af2cd
harshedabdulla <harshed2004@gmail.com>
2022-08-26 20:33:56 +0530

    doing main

--- Arrays/Binary Search/pb1.c
+++ Arrays/Binary Search/pb1.c
@@ -12,5 +12,34 @@ int main(){
         scanf("%d",&a[i]);
     }
     //if elements are not sorted
-
+    for (int j = 0; j < n; j++)
+    {
+        if (a[j]>a[j+1])
+        {
+            /* code */
+            printf("Not sorted properly\n");
+            return 0;
+        }
+        
+    }
+    int key;
+    scanf("%d",&key);
+    int low=0,high=n-1,mid=(low+high)/2;
+    while (low<=high)
+    {
+        /* code */
+        if (a[mid]<key)
+        {
+            /* code */
+            low=mid+1;
+        }else if (a[mid]==key)
+        {
+            /* code */
+            
+        }
+        
+        
+    }
+    
+    
 }
