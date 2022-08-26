77eb67eb64add0a2097dfd2d45f0adf0c20b0273
harshedabdulla <harshed2004@gmail.com>
2022-08-26 08:07:16 +0530

    pb12

---
+++ Lab Programs/Cycle 2/Arrays/pb12.c
@@ -0,0 +1,20 @@
+/** Program to print the sum and average of elements in an array
+ * 
+ * ALGORITHM
+ * 
+ * START
+ * 
+ * 
+ * 
+ * STOP
+* */
+#include<stdio.h>
+int main(){
+    int arr[6];
+    for (int i = 0; i < 6; i++)
+    {
+        printf("Enter the %dth element: ",i);
+        scanf("%d",&arr[i]);
+    }
+    
+}
