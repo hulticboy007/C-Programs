77a5764d14650ed796f87447d98a2a0eeaed3449
harshedabdulla <harshed2004@gmail.com>
2022-08-26 22:33:19 +0530

    dsfsdf

---
+++ Sorting/Bubble sort/pb1.c
@@ -0,0 +1,14 @@
+// program to sort numbers in an array using bubble sort
+
+int main(){
+    int n;
+    printf("Enter the array length: ");
+    scanf("%d",&n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the %d element: ");
+        scanf("%d",a[i]);
+    }
+    
+}
