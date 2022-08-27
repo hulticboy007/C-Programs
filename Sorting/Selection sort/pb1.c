6ff91238b0f2a1d6d4816da73baf62f37e692130
harshedabdulla <harshed2004@gmail.com>
2022-08-27 16:05:44 +0530

    changes

--- Sorting/Selection sort/pb1.c
+++ Sorting/Selection sort/pb1.c
@@ -2,13 +2,41 @@
 
 #include<stdio.h>
 int main(){
-    int n;
+    int n,i,j,p,s;
     printf("Enter the array length: ");
     scanf("%d",&n);
     int a[n];
-    for (int i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
         printf("Enter the %d element: ",i);
         scanf("%d",&a[i]);
     }
+    for ( i = 0; i < n-1; i++)
+    {
+        p=i;
+    }
+    for ( j = i+1; j < n; j++)
+    {
+        /* code */
+        if (a[p]>a[j])
+        {
+            /* code */
+            p=j;
+        }
+        if (p!=i)
+        {
+            /* code */
+            s=a[i];
+            a[i]=a[p];
+            a[p]=s;
+        }
+        
+    }
+    printf("Sorted Array: \n");
+    for ( i = 0; i < n; i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    return 0;
+    
 }
