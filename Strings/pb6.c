cb2957f5b9386b7fccffd9a93052498ed10abc55
harshedabdulla <harshed2004@gmail.com>
2022-08-28 20:43:03 +0530

    pb6

--- Strings/pb6.c
+++ Strings/pb6.c
@@ -5,5 +5,26 @@
 #include<string.h>
 
 int main(){
-
-}
+    int flag=0;
+    char s1[30]="Abdulla";
+    char s2[30]="Abdulla";
+    for (int i = 0; s1[i]!='\0'||s2[i]!='\0'; i++)
+    {
+        /* code */
+        if (s1[i]!=s2[i])
+        {
+            /* code */
+            flag=1;
+            break;
+        }
+    }
+    if (flag==1)
+    {
+        /* code */
+        printf("Not same");
+    }else{
+        printf("Same");
+    }
+    
+    }
+    
