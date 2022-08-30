068c54194d4729a6566d51b7d230c790a5845af1
harshedabdulla <harshed2004@gmail.com>
2022-08-30 22:08:34 +0530

    pb37

---
+++ TCS-NQT/Chapter-2/pb37.c
@@ -0,0 +1,11 @@
+//Program to show input and output of a string.
+
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char in[50];
+    printf("Enter the string: ");
+    gets(in);
+    puts(in);
+}
