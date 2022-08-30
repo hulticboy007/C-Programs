7ab78d3a2de8291ad89941c51bc0b28a9687f3cb
harshedabdulla <harshed2004@gmail.com>
2022-08-30 22:03:08 +0530

    dasda

--- Functions/pb3.c
+++ Functions/pb3.c
@@ -1,8 +1,15 @@
 //program to use call by value , function which use actual numbers in actual parameter when calling.
 
 #include<stdio.h>
-int main(){
-    
+void fun(int,int);
 
-
+void main(){
+    int x=5,y=7;
+    fun(x,y);
+    printf("x: %d, y: %d ",x,y);
+}
+void fun(int x,int y){
+    x=7;
+    y=5;
+    printf("x: %d, y: %d ",x,y);
 }
