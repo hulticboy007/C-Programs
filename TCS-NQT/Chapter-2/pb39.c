44400608c5fd477a1681590c920562de019093c5
harshedabdulla <harshed2004@gmail.com>
2022-08-31 23:41:55 +0530

    pb39

--- TCS-NQT/Chapter-2/pb39.c
+++ TCS-NQT/Chapter-2/pb39.c
@@ -1,2 +1,17 @@
 //program to swap two numbers using functions
 
+#include<stdio.h>
+void swap(int a,int b){
+    int temp=0;
+    temp=a;
+    a=b;
+    b=temp;
+    printf("a: %d b: %d",a,b);
+}
+int main(){
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    swap(a,b);
+
+}
