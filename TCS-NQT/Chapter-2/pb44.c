99a4fbb8e33b9a42f101a6259763aa7a4bb6cabc
harshedabdulla <harshed2004@gmail.com>
2022-09-08 22:20:12 +0530

    not working

--- TCS-NQT/Chapter-2/pb44.c
+++ TCS-NQT/Chapter-2/pb44.c
@@ -1,16 +1,18 @@
 // program to find the largest of two numbers using functions
 
 #include<stdio.h>
-int max(int,int);
+void max(int,int);
 int main(){
     int x,y;
     printf("Enter the two numbers");
+    scanf("%d %d",x,y);
     max(x,y);
 }
-int max(int x,int y){
+void max(int x,int y){
     if(x>y){
         printf("%d",x);
     }else{
-        printf("%d",y;)
+        printf("%d",y);
     }
+    
 }
