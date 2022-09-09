7ccdda784dcf14dffe3f46ba0338f4dac1f30089
harshedabdulla <harshed2004@gmail.com>
2022-09-09 15:27:16 +0530

    7.4

--- Lab Programs/Cycle 2/Strings/pb7.c
+++ Lab Programs/Cycle 2/Strings/pb7.c
@@ -4,7 +4,7 @@ comparison, find the length and string copy without using library functions. */
 #include<string.h>
 
 int main(){
-    
+
     printf("\n Concatanation,Comparison,Length and Copy without using function \n\n");
     int i,point=0,flag=0;
     char first[30],sec[30],l1,l2;
@@ -12,14 +12,6 @@ int main(){
     scanf("%s",first);
     printf("Enter the second string: ");
     scanf("%s",sec);
-    printf("\n String Concatanation : ");
-    l1=strlen(first);
-    l2=strlen(sec);
-    for (i = 0; i <= l2; i++)
-    {
-        first[l1+i]=sec[i];
-    }
-    printf("%s",first);
     printf("\n String Comparison: ");
     for (i = 0; first[i]!='\0' || sec[i]!='\0'; i++)
     {
@@ -37,8 +29,20 @@ int main(){
     {
         printf("Not same, similarity broke at %d ",point);
     }else{
-        printf("Not same");
+        printf("Same");
     }
+
+    printf("\n String Length : ");
+    
+    printf("\n String Concatanation : ");
+    l1=strlen(first);
+    l2=strlen(sec);
+    for (i = 0; i <= l2; i++)
+    {
+        first[l1+i]=sec[i];
+    }
+    printf("%s",first);
+    
     
     
 }
