2d41b6c911ff4f9b4ad8ffdf6abcc7ea7bc36807
harshedabdulla <harshed2004@gmail.com>
2022-09-09 23:11:01 +0530

    1.4

--- Lab Programs/Cycle 3/Functions/pb1.c
+++ Lab Programs/Cycle 3/Functions/pb1.c
@@ -1,11 +1,11 @@
 // To write a C program to calculate the mean, variance and standard deviation using functions.
 
 #include<stdio.h>
-
+#include<math.h>
 
 int main()
 {
-    int n,sum=0,mean=0,sd,div;
+    int n,sum=0,mean=0,var,sd,df;
     printf("Enter the array length: ");
     scanf("%d",&n);
     int arr[n];
@@ -19,6 +19,17 @@ int main()
         sum=sum+arr[j];
     }
     printf("Sum is %d\n",sum);
+    mean=sum/n;
+    for (int k = 0; k < n; k++)
+    {
+        df=df+(mean-arr[k]);
+    }
+    var=(df*df)/n;
+    sd=sqrt(var);
+    printf("Mean is %d\n",mean);
+    printf("Variance is %d\n",var);
+    printf("Standard deviation is %d\n",sd);
+    
     
     
 }
