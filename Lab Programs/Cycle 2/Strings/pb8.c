4b6272bdc514d18a13ad0e8fafa2e3a8f18928e5
harshedabdulla <harshed2004@gmail.com>
2022-09-09 16:12:24 +0530

    8.3

--- Lab Programs/Cycle 2/Strings/pb8.c
+++ Lab Programs/Cycle 2/Strings/pb8.c
@@ -1,16 +1,29 @@
 //program to arrang names in alphabetical order
 
 #include<stdio.h>
+#include<string.h>
 
 int main(){
-    int n;
+    int n,i,j;
     printf("no. of names: ");
     scanf("%d",&n);
-    char names[n][100];
-    for (int i = 0; i <n; i++)
+    char names[n][100],s[100];
+    for (i = 0; i <n; i++)
     {
         scanf("%s",names[i]);
     }
-    
+    for(i=0;i<n;i++){
+      for(j=i+1;j<n;j++){
+         if(strcmp(names[i],names[j])>0){
+            strcpy(s,names[i]);
+            strcpy(names[i],names[j]);
+            strcpy(names[j],s);
+         }
+      }
+   }
+   printf("\nThe sorted order of names are:\n");
+   for(i=0;i<n;i++){
+      printf("%s\n",names[i]);
+   }
     
 }
