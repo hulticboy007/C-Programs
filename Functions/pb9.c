b7faf6182e83ccee124b6cb1788c85cb560a4b33
harshedabdulla <harshed2004@gmail.com>
2022-09-10 19:46:08 +0530

    9.1

--- Functions/pb9.c
+++ Functions/pb9.c
@@ -10,5 +10,15 @@ int main(){
     scanf("%s",str1);
     printf("Enter the second string: ");
     scanf("%s",str2);
-    
+    mod(str1,str2);
+}
+void mod(char str1[],char str2[]){
+    int c=0,i;
+    for (i= 0;str1[i]!='\0'; i++)
+    {
+        c=c+1;
+    }
+    printf("The length of first string is %d\n",c);
+    str2[0]='A';
+    printf("The second string is %s",str2);
 }
