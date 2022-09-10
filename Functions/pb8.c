639225fe1e8d377223db84d05c02ad63462e8730
harshedabdulla <harshed2004@gmail.com>
2022-09-10 19:19:45 +0530

    8.0

---
+++ Functions/pb8.c
@@ -0,0 +1,14 @@
+// program to pass string as an argument
+
+#include<stdio.h>
+
+void display(char[]);
+int main(){
+    char string[]="harshed";
+    // printf("Enter the string: ");
+    // scanf("%s",string);
+    display(string);
+}
+void display(char string[]){
+    printf("The inputted string is %s",string);
+}
