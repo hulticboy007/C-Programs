0731854ea67e632e737cd1d62593d74c54243547
harshedabdulla <harshed2004@gmail.com>
2022-09-11 08:42:41 +0530

    10.1

---
+++ Functions/pb10.c
@@ -0,0 +1,13 @@
+// program to return string in function
+
+#include<stdio.h>
+
+char* harshed(){
+    return "abdulla is king!";
+}
+
+int main(){
+    char hack[]=harshed();
+    
+    printf("%s",hack);
+}
