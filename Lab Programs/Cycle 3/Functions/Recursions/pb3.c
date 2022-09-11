176eddb9ff542ea400b8a6ed2ae6b44e13513808
harshedabdulla <harshed2004@gmail.com>
2022-09-11 18:43:51 +0530

    3.1

---
+++ Lab Programs/Cycle 3/Functions/Recursions/pb3.c
@@ -0,0 +1,20 @@
+// program to print factorial using recursion
+
+#include<stdio.h>
+int fact(int a){
+    if (a==0)
+    {
+        return 1;
+    }else{
+        return (a*fact(a-1));
+    }
+}   
+    
+int main(){
+    int n,k;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    k=fact(n);
+    printf("%d",k);
+
+}
