37185b9a4172874f3e27c6f437ef382f23357025
harshedabdulla <harshed2004@gmail.com>
2022-09-12 20:02:59 +0530

    1.1

---
+++ Examples/pb1.c
@@ -0,0 +1,10 @@
+// program to learn about typecasting
+
+#include<stdio.h>
+
+int main(){
+    int a=3;
+    printf("value of a is %f\n",(float)a);
+    float b=(float)918/10;
+    printf("value of b is %f",b);
+}
