08b34d59c36ca1c3f2dbde3219060b6e67a906dd
harshedabdulla <harshed2004@gmail.com>
2022-09-14 22:22:28 +0530

    7 and 8

---
+++ Practise/pb8.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void fiba(int z){
+    static int n1=0,n2=1,n3;
+    if (z>0)
+    {
+        n3=n1+n2;
+        n1=n2;
+        n2=n3;
+        printf(" %d ",n3);
+        fiba(z-1);
+    }
+    
+}
+int main(){
+    int n;
+    printf("number: ");
+    scanf("%d",&n);
+    printf(" 1 ");
+    fiba(n-1);
+    return 0;
+}
