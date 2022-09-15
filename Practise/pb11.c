30024b0ffd1a19d38a4bb0fbb4f3d703ddfbb9b0
harshedabdulla <harshed2004@gmail.com>
2022-09-15 21:44:21 +0530

    finally

---
+++ Practise/pb11.c
@@ -0,0 +1,13 @@
+// program to multiply two floating point numbers
+
+#include<stdio.h>
+
+int main(){
+    float f1,f2,m;
+    printf("Enter float 1: ");
+    scanf("%f",&f1);
+    printf("Enter float 2: ");
+    scanf("%f",&f2);
+    m=f1*f2;
+    printf("Multiplication: %f \n",m);
+}
