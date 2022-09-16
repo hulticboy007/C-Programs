349e1fdcb40ef78837bd2b4f1016382449a1b63a
harshedabdulla <harshed2004@gmail.com>
2022-09-16 22:59:56 +0530

    2.2

--- Files/pb2.c
+++ Files/pb2.c
@@ -1,7 +1,33 @@
 // program to copy string from one file another
 
 #include<stdio.h>
-
+#include<stdlib.h>
 int main(){
-    
+    FILE *fs,*ft;
+    char ch;
+    fs=fopen("test.txt","r");
+    if (fs==NULL)
+    {
+        printf("Invalid file");
+        exit(1);
+    }
+    ft=fopen("test2.txt","w");
+    if (ft==NULL)
+    {
+        printf("Invalid");
+        exit(2);
+    }
+    while (1)
+    {
+        ch=fgetc(fs);
+        if(ch==EOF){
+            break;
+        }else{
+            fputc(ch,ft);
+        }
+    }
+    fclose(fs);
+    fclose(ft);
+    return 0;
 }
+    
